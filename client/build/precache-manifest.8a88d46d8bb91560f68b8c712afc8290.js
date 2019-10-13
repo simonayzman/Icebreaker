1e45fe7361c27f8f3ed7d57037c1ad9b61d24309
Simon Ayzman <simonayzman@gmail.com>
2019-10-13 17:39:35 -0500

    Unignored the build directory

---
+++ client/build/precache-manifest.8a88d46d8bb91560f68b8c712afc8290.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a8778f73c338f979b769e8782e1d00f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b34d314c8be9461d1a82",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "14f7ba62d534ca10bdba",
+    "url": "/static/js/2.8e9d5bf3.chunk.js"
+  },
+  {
+    "revision": "b34d314c8be9461d1a82",
+    "url": "/static/js/main.c03f2511.chunk.js"
+  },
+  {
+    "revision": "cbee8d858c084296ccff",
+    "url": "/static/js/runtime-main.985e6003.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
