9d76a7e6daed327e5052021f6384bd5229255661
Simon Ayzman <simonayzman@gmail.com>
2019-10-14 21:58:14 -0500

    Added env sensitivity to python server

--- server/main.py
+++ server/main.py
@@ -4,13 +4,39 @@ from flask import Flask, render_template, send_from_directory
 from flask_socketio import SocketIO, emit, join_room
 from flask_cors import CORS
 from flask_debug import Debug
+import json
+
+configs = {
+    "development": {
+        "client": {
+            "token": "Hello DEV Flask",
+            "api": "http://localhost",
+            "port": 5000,
+        },
+        "server": {
+            "static": "../client/build",
+            "template": "../client/build",
+        },
+    },
+    "production": {
+        "client": {
+            "token": "Hello PROD Flask",
+            "api": "https://deep-dive-072193.herokuapp.com",
+            "port": environ.get('PORT'),
+        },
+        "server": {
+            "static": "../client/build/static",
+            "template": "../client/build",
+        },
+    },
+   
+}
 
 app = Flask(
-    "__main__",
-    static_folder="../client/build/static",
-    template_folder="../client/build"
+    __name__,
+    static_folder=configs[environ.get('FLASK_ENV')]['server']["static"],
+    template_folder=configs[environ.get('FLASK_ENV')]['server']["template"],
 )
-
 Debug(app)
 CORS(app)
 socketio = SocketIO(app)
@@ -18,11 +44,12 @@ socketio = SocketIO(app)
 # App routes
 @app.route("/")
 def index():
-    return render_template("index.html", token="Hello flask")
+    config_json_string = json.dumps(configs[environ.get('FLASK_ENV')]['client'])
+    return render_template("index.html", config=config_json_string)
 
 @app.route("/<path:path>")
 def indexPath(path):
-    return send_from_directory("../client/build", path)
+    return send_from_directory(configs[environ.get('FLASK_ENV')]['server']["static"], path)
 
 # Socket connections
 @socketio.on('connect')
