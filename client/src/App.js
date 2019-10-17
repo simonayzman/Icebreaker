414bc04f231798524d28eefd58e3254bb8111898
Simon Ayzman <simonayzman@gmail.com>
2019-10-17 15:34:33 -0500

    Linted react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import io from "socket.io-client";
+import io from 'socket.io-client';
 
 import logo from './logo.svg';
 import './App.css';
 
-const CONFIG = window.config || { token: "Hello DEV Flask", api: "http://localhost", port: 8000 };
+const CONFIG = window.config || { token: 'Hello DEV Flask', api: 'http://localhost', port: 8000 };
 const API = `${CONFIG.api}:${CONFIG.port}`;
 
 const socket = io();
 
 export default class App extends Component {
   componentDidMount() {
-    socket.on("connect", data => console.log("Socket connected on the front-end."));
-    setInterval(() => socket.emit("client_request", { user: window.location.href }), 5000)
+    socket.on('connect', data => console.log('Socket connected on the front-end.'));
+    setInterval(() => socket.emit('client_request', { user: window.location.href }), 5000);
   }
 
   render() {
@@ -23,9 +23,7 @@ export default class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <p>
-            {CONFIG.token}
-          </p>
+          <p>{CONFIG.token}</p>
           <a
             className="App-link"
             href="https://reactjs.org"
