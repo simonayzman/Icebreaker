f89c727c74e1d892725c854d060547ad1ddf60e1
Simon Ayzman <simonayzman@gmail.com>
2019-11-09 14:43:37 -0600

    Updated backend with room creation/joining

--- server/main.py
+++ server/main.py
@@ -1,5 +1,5 @@
 from os import environ
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request, jsonify
 from flask_socketio import SocketIO, emit, join_room
 from flask_cors import CORS
 from flask_debug import Debug
@@ -75,6 +75,39 @@ def indexPath(path):
     return send_from_directory(getConfig()["template"], path)
 
 
+@app.route("/createRoom")
+def createRoomPath():
+    roomCode = request.args.get("roomCode")
+    initialData = {"users": {}, "matches": {}}
+
+    response = None
+    try:
+        rooms_ref.document(roomCode).set(initialData)
+        response = {"ok": True}
+    except:
+        response = {"error": True}
+
+    print(f"Room creation for code {roomCode} yielded response: {response}")
+
+    return jsonify(response)
+
+
+@app.route("/checkRoom")
+def checkRoomPath():
+    roomCode = request.args.get("roomCode")
+    room = rooms_ref.document(roomCode).get().to_dict()
+
+    response = None
+    if room is None:
+        response = {"error": True}
+    else:
+        response = room
+
+    print(f"Looking for room {roomCode} yielded response: {response}")
+
+    return jsonify(response)
+
+
 # Socket connections
 @socketio.on("connect")
 def on_connect():
@@ -84,11 +117,13 @@ def on_connect():
 @socketio.on("join_room")
 def on_client_request(data):
     print(f"Joining room with data: {data}")
-    rooms_ref.document(data["room"]).set(data)
-    emit(
-        "join_room_success",
-        {"success": True, "time": datetime.now().strftime("%H:%M:%S")},
-    )
+    roomCode = data["roomCode"]
+    user = data["user"]
+    userId = user["userId"]
+    rooms_ref.document(roomCode).update({f"users.{userId}": user})
+    emit("join_room_success", user)
+
+
 
 
 # @socketio.on("join_room")
