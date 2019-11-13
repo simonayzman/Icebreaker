bd94162291871cccc994c98451da72725b725fbf
Simon Ayzman <simonayzman@gmail.com>
2019-11-13 00:45:39 -0600

    Updated questions

--- client/src/lib/questions.js
+++ client/src/lib/questions.js
@@ -1,45 +1,33 @@
 const questions = {
   q1: {
     questionId: 'q1',
-    prompt:
-      'Do you think that people should have the freedom to blah blah blah?  1',
+    prompt: 'Do you think that people should have the freedom to blah blah blah?  1',
     topics: ['testTopic1', 'testTopic2'],
+    image: 'politics',
   },
   q2: {
     questionId: 'q2',
-    prompt:
-      'Do you think that people should have the freedom to blah blah blah?  2',
+    prompt: 'Do you think that people should have the freedom to blah blah blah?  2',
     topics: ['testTopic2'],
+    image: 'music',
   },
   q3: {
     questionId: 'q3',
-    prompt:
-      'Do you think that people should have the freedom to blah blah blah?  3',
+    prompt: 'Do you think that people should have the freedom to blah blah blah?  3',
     topics: ['testTopic4', 'testTopic5'],
+    image: 'family',
   },
   q4: {
     questionId: 'q4',
-    prompt:
-      'Do you think that people should have the freedom to blah blah blah?  4',
+    prompt: 'Do you think that people should have the freedom to blah blah blah?  4',
     topics: ['testTopic4'],
+    image: 'food',
   },
   q5: {
     questionId: 'q5',
-    prompt:
-      'Do you think that people should have the freedom to blah blah blah?  5',
+    prompt: 'Do you think that people should have the freedom to blah blah blah?  5',
     topics: ['testTopic5'],
-  },
-  q6: {
-    questionId: 'q6',
-    prompt:
-      'Do you think that people should have the freedom to blah blah blah?  6',
-    topics: ['testTopic1'],
-  },
-  q7: {
-    questionId: 'q7',
-    prompt:
-      'Do you think that people should have the freedom to blah blah blah?  7',
-    topics: ['testTopic3'],
+    image: 'coding',
   },
 };
 
