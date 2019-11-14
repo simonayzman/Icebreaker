b848557771a3b5a753cea6e5048ec00214734b47
Simon Ayzman <simonayzman@gmail.com>
2019-11-13 20:29:01 -0600

    Updated build

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.004b2604490cb76d2f2283758a79d912.js"
+  "/precache-manifest.40b16691987c43b5f7bae429491863d5.js"
 );
 
 self.addEventListener('message', (event) => {
