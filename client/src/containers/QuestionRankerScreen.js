cae3052a90e11bc1516bf80f6656bc7848df9cd0
Simon Ayzman <simonayzman@gmail.com>
2019-11-13 19:24:43 -0600

    Updated basic question ranker screen

--- client/src/containers/QuestionRankerScreen.js
+++ client/src/containers/QuestionRankerScreen.js
@@ -1,140 +1,121 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 // import { ScrollView, StyleSheet, View, FlatList, TouchableOpacity, SafeAreaView } from 'react-native';
 // import { Button, Text, ListItem, Icon } from 'react-native-elements';
 // import Swiper from 'react-native-deck-swiper';
+import { Button } from 'react-bootstrap';
 
-// import SAMPLE_QUESTIONS from '../constants/questions';
+import QUESTIONS, { ORDERED_QUESTIONS } from '../lib/questions';
 // import SAMPLE_MATCHES from '../constants/matches';
 // import SAMPLE_USERS from '../constants/users';
 // import { add_user, add_room } from '../Firestore';
 
-export default class CardSwipeScreen extends Component {
-  /*
-    constructor(props) {
-        super(props);
-        this.questionList = props.navigation.state.params.questionList;
-        this.state = {
-            questionRankings: {},
-            inProgressQuestionRankings: {},
-            allUsers: Object.keys(SAMPLE_USERS),
-            currentUsers: Object.keys(SAMPLE_USERS).splice(2),
-            users: SAMPLE_USERS,
-            matches: SAMPLE_MATCHES,
-            finishedQuestions: false,
-            numOfQuestionsLeft: Object.keys(this.questionList).length,
-            questions: this.questionList
-        };
-    }
+const QuestionRankerScreenContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+`;
 
-    componentDidMount() {
-        console.log("Nav params is: " + JSON.stringify(this.props.navigation.state.params))
-    }
+export default class QuestionRankerScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      questionRankings: {},
+      inProgressQuestionRankings: {},
+      cardIndex: 0,
+    };
+  }
 
-    onSwipe = (cardIndex, ranking) => {
-        const { inProgressQuestionRankings } = this.state;
-        const orderedQuestions = Object.values(this.questionList)
-        const numOfQuestions = orderedQuestions.length;
-        const numOfQuestionsLeft = numOfQuestions - 1 - cardIndex;
-        this.setState({
-            inProgressQuestionRankings: {
-                ...inProgressQuestionRankings,
-                questionId: ranking,
-            },
-            numOfQuestionsLeft: numOfQuestionsLeft,
-            // questions: questions
-        });
+  onRank = ranking => {
+    const { onRankAll } = this.props;
+    const { inProgressQuestionRankings, cardIndex } = this.state;
+    const currentQuestionId = ORDERED_QUESTIONS[cardIndex];
+    const newCardIndex = cardIndex + 1;
+    const updatedInProgressQuestionRankings = {
+      ...inProgressQuestionRankings,
+      [currentQuestionId]: ranking,
     };
 
-    onSwipedAll = () => {
-        const { inProgressQuestionRankings } = this.state;
-        this.setState({
-            questionRankings: inProgressQuestionRankings,
-            inProgressQuestionRankings: {},
-            finishedQuestions: true
-        });
-    };
+    if (newCardIndex === Object.keys(QUESTIONS).length) {
+      // Done!
+      this.setState({
+        questionRankings: updatedInProgressQuestionRankings,
+        inProgressQuestionRankings: {},
+        cardIndex: newCardIndex,
+      });
+      onRankAll(updatedInProgressQuestionRankings);
+    } else {
+      // Some more left to go!
+      this.setState({
+        cardIndex: newCardIndex,
+        inProgressQuestionRankings: updatedInProgressQuestionRankings,
+      });
+    }
+  };
 
-    renderCard = questionId => {
-        const { questions } = this.state;
-        const question = questions[questionId];
-        return (
-            <View style={styles.card}>
-                <Text style={styles.text}>{question.prompt}</Text>
-            </View>
-        );
-    };
-
-    enterRoomScreen = async () => {
-        const { userId, codeName, roomId, description, roomState, roomName } = this.props.navigation.state.params;
-
-        console.log('!!!!', userId, roomId, codeName, description);
-
-        if (roomState != "join") {
-            await add_room(roomId, roomName);
-        }
-        await add_user(roomId, {
-            userId,
-            codeName,
-            description,
-            questionRankings: this.state.questionRankings
-        });
-
-        this.props.navigation.replace("Room", {
-            userId,
-            roomId,
-            codeName
-        })
+  renderCard = cardIndex => {
+    if (cardIndex === ORDERED_QUESTIONS.length) {
+      return (
+        <div style={styles.card}>
+          <div style={styles.text}>{"You're done!"}</div>
+        </div>
+      );
     }
 
-    render() {
-        const { questions } = this.state;
+    const currentQuestionId = ORDERED_QUESTIONS[cardIndex];
+    const currentQuestion = QUESTIONS[currentQuestionId];
+    const { prompt, image } = currentQuestion;
+    return (
+      <div style={styles.card}>
+        <img src={require(`../assets/${image}.png`)} style={{ width: '100%', height: 'auto' }} />
+        <div style={styles.text}>{prompt}</div>
+      </div>
+    );
+  };
 
-        console.log(questions)
-        let orderedQuestions = Object.keys(questions)
-        const numOfQuestions = orderedQuestions.length;
-
-
-        return (
-            <SafeAreaView style={styles.container}>
-                {
-                    this.state.numOfQuestionsLeft > 0
-                    &&
-                    <View style={{ alignItems: "center" }}>
-                        <Text>Number of Questions Left</Text>
-                        <Text>{`${this.state.numOfQuestionsLeft} / ${numOfQuestions}`}</Text>
-                    </View>
-                }
-                <View style={{ flex: 1 }} >
-                    {
-                        this.state.finishedQuestions ?
-                            <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-                                <Text>You're done with all your questions</Text>
-                                <TouchableOpacity style={styles.joinButton} onPress={this.enterRoomScreen}>
-                                    <Text style={{ color: "#fff" }}>Join Room</Text>
-                                </TouchableOpacity>
-                            </View>
-                            :
-                            <Swiper
-                                cardIndex={0}
-                                stackSize={3}
-                                useViewOverflow={false}
-                                verticalSwipe={false}
-                                cards={orderedQuestions}
-                                renderCard={this.renderCard}
-                                onSwipedLeft={cardIndex => this.onSwipe(cardIndex, 'dislike')}
-                                onSwipedRight={cardIndex => this.onSwipe(cardIndex, 'like')}
-                                onSwipedAll={this.onSwipedAll}
-                                cardHorizontalMargin={0}
-                                backgroundColor={'#F5FCFF'}
-                            />
-                    }
-                </View>
-            </SafeAreaView >
-        );
-    }
-    */
   render() {
-    return <h2>{'QUESTION RANKER!'}</h2>;
+    const { cardIndex } = this.state;
+    return (
+      <QuestionRankerScreenContainer>
+        <>{this.renderCard(cardIndex)}</>
+        <>
+          <Button onClick={() => this.onRank('dislike')}>{'NO'}</Button>
+          <Button onClick={() => this.onRank('indifferent')}>{'EH'}</Button>
+          <Button onClick={() => this.onRank('like')}>{'YES'}</Button>
+          <Button onClick={() => this.onRank('superlike')}>{'OH YES!'}</Button>
+        </>
+        {/* {this.state.numOfQuestionsLeft > 0 && (
+          <View style={{ alignItems: 'center' }}>
+            <Text>Number of Questions Left</Text>
+            <Text>{`${this.state.numOfQuestionsLeft} / ${numOfQuestions}`}</Text>
+          </View>
+        )}
+        <View style={{ flex: 1 }}>
+          {this.state.finishedQuestions ? (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+              <Text>You're done with all your questions</Text>
+              <TouchableOpacity style={styles.joinButton} onPress={this.enterRoomScreen}>
+                <Text style={{ color: '#fff' }}>Join Room</Text>
+              </TouchableOpacity>
+            </View>
+          ) : (
+            <Swiper
+              cardIndex={0}
+              stackSize={3}
+              useViewOverflow={false}
+              verticalSwipe={false}
+              cards={orderedQuestions}
+              renderCard={this.renderCard}
+              onSwipedLeft={cardIndex => this.onSwipe(cardIndex, 'dislike')}
+              onSwipedRight={cardIndex => this.onSwipe(cardIndex, 'like')}
+              onSwipedAll={this.onSwipedAll}
+              cardHorizontalMargin={0}
+              backgroundColor={'#F5FCFF'}
+            />
+          )}
+        </View> */}
+      </QuestionRankerScreenContainer>
+    );
   }
 }
 
@@ -161,7 +142,7 @@ const styles = {
   text: {
     textAlign: 'left',
     fontSize: 32,
-    backgroundColor: 'transparent',
+    color: 'black',
   },
   joinButton: {
     marginTop: 25,
