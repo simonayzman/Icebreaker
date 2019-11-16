bb52cdfe15feb60d7e0fe324ec976e155163ea87
Simon Ayzman <simonayzman@gmail.com>
2019-11-15 22:41:26 -0600

    Updated questions

--- client/src/lib/questions.js
+++ client/src/lib/questions.js
@@ -1,35 +1,40 @@
 const QUESTIONS = {
   q1: {
     questionId: 'q1',
-    prompt: 'Do you think that people should have the freedom to blah blah blah?  1',
-    topics: ['testTopic1', 'testTopic2'],
+    prompt: "What's more important: the individual or the collective good?",
     image: 'politics',
   },
   q2: {
     questionId: 'q2',
-    prompt: 'Do you think that people should have the freedom to blah blah blah?  2',
-    topics: ['testTopic2'],
+    prompt: 'How has your taste in music changed over time?',
     image: 'music',
   },
   q3: {
     questionId: 'q3',
-    prompt: 'Do you think that people should have the freedom to blah blah blah?  3',
-    topics: ['testTopic4', 'testTopic5'],
+    prompt: 'What impact has family had on your idea of success?',
     image: 'family',
   },
   q4: {
     questionId: 'q4',
-    prompt: 'Do you think that people should have the freedom to blah blah blah?  4',
-    topics: ['testTopic4'],
+    prompt: "What's the best meal you've ever had?",
     image: 'food',
   },
   q5: {
     questionId: 'q5',
-    prompt: 'Do you think that people should have the freedom to blah blah blah?  5',
-    topics: ['testTopic5'],
-    image: 'coding',
+    prompt: 'Has social media made the world a better place?',
+    image: 'internet',
+  },
+  q6: {
+    questionId: 'q6',
+    prompt: 'What gives you peace of mind?',
+    image: 'meditation',
+  },
+  q7: {
+    questionId: 'q7',
+    prompt: 'Where does career growth rank in your list of priorities?',
+    image: 'career',
   },
 };
 
-export const ORDERED_QUESTIONS = Object.keys(QUESTIONS);
+export const ORDERED_QUESTIONS = ['q3', 'q4', 'q5', 'q6', 'q7', 'q1', 'q2'];
 export default QUESTIONS;
